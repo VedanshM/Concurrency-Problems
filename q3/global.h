4e2b38d4d9c16b24f741b4c221dae6cf1bf25ff7
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-10-17 23:23:12 +0530

    q3: basic structure

---
+++ q3/global.h
@@ -0,0 +1,96 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#define _POSIX_C_SOURCE 200112L
+#include <inttypes.h>
+#include <limits.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <wait.h>
+
+#define ELECTRIC_STAGE 0
+#define ACOUSTIC_STAGE 1
+#define BOTH_STAGE 2
+
+typedef enum {
+	NOT_ARRIVED,
+	WAITING_TO_PERFORM,
+	PERFORMING_SOLO,
+	PERFORMING_W_SINGER,
+	WAITING_FOR_TS,
+	COLLECTING_TS,
+	EXITED,
+} musician_state_t;
+
+typedef enum {
+	SINGER,
+	PIANO,
+	GUITAR,
+	VOILIN,
+	BASS,
+} instrument_t;
+
+char **instr_name = {
+	"SINGER", "PIANO", "GUITAR", "VOILIN", "BASS"};
+
+typedef struct musician {
+	char name[1024];
+	musician_state_t state;
+	instrument_t instr;
+	int stage_type;
+	int arrival_time;
+	pthread_t tid;
+} musician_t;
+
+typedef struct stage {
+	int id;
+	int musician_performing;
+	int singer_performing;
+	pthread_mutex_t mutex;
+} stage_t;
+
+pthread_mutex_t global_mutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t ac_sem, ec_sem, both_sem;
+int no_of_musicians, acoustic_stg_cnt, electric_stg_cnt, coord_cnt;
+int impatience_limit, t1, t2;
+stage_t *ac_stages, *ec_stages;
+
+musician_t *musicians;
+
+int randInt(int a, int b) { //generate random int in [a,b]
+	return a + rand() % (b - a + 1);
+}
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+
+void musicians_init(int n);
+void *musician_thread(void *);
+
+int perform_on_type(musician_t* msc, stage_t* stages, int stg_cnt)
+
+//   Colour escape codes
+#define BLK_COL "\033[30;1m"
+#define RED_COL "\033[31;1m"
+#define GRN_COL "\033[32;1m"
+#define YLW_COL "\033[33;1m"
+#define BLU_COL "\033[34;1m"
+#define MGN_COL "\033[35;1m"
+#define CYN_COL "\033[36;1m"
+#define WHT_COL "\033[37;1m"
+#define RST_COL "\033[0m"
+
+#endif
---
+++ q3/musician.h
@@ -0,0 +1,119 @@
+#ifndef MUSICIAN_H
+#define MUSICIAN_H
+
+#include "global.h"
+
+void musicians_init(int n) {
+	musicians = (musician_t *)malloc(n * sizeof(musician_t));
+	for (int i = 0; i < n; i++) {
+		char c;
+		scanf("%s %c %d", musicians[i].name, c, musicians[i].arrival_time);
+		switch (c) {
+		case 's':
+			musicians[i].instr = SINGER;
+			break;
+		case 'p':
+			musicians[i].instr = PIANO;
+			break;
+		case 'g':
+			musicians[i].instr = GUITAR;
+			break;
+		case 'v':
+			musicians[i].instr = VOILIN;
+			break;
+		case 'b':
+			musicians[i].instr = BASS;
+			break;
+
+		default:
+			printf("wrong instrument . setting musician to singer");
+			musicians[i].instr = SINGER;
+			break;
+		}
+		musicians[i].state = NOT_ARRIVED;
+	}
+	for (int i = 0; i < n; i++)
+		pthread_create(&(musicians[i].tid), NULL, musician_thread, musicians + i);
+}
+
+void *musician_thread(void *ptr) {
+	musician_t *msc = (musician_t *)ptr;
+	sleep(msc->arrival_time);
+
+	int performed = 0;
+	struct timespec ts;
+	ts->tv_sec = impatience_limit;
+	ts->tv_nsec = 0;
+	if (msc->stage_type == ELECTRIC_STAGE) {
+		if (sem_timedwait(&ec_sem, &ts) == 0) { //waiting for a EC stage  to get free
+												//one of the stage has to be empty is sem allowed
+			perform_on_type(msc, ec_stages, electric_stg_cnt);
+			sem_post(&ec_sem);
+			performed = 1;
+		}
+	} else if (msc->stage_type == ACOUSTIC_STAGE) {
+		if (sem_timedwait(&ac_sem, &ts) == 0) { //waiting for a AC stage  to get free
+												//one of the stage has to be empty is sem allowed
+			perform_on_type(msc, ac_stages, acoustic_stg_cnt);
+			sem_post(&ac_sem);
+			performed = 1;
+		}
+	} else if (msc->stage_type == BOTH_STAGE) {
+		if (sem_timedwait(&both_sem, &ts) == 0) { //waiting for any stage  to get free
+												  //one of the stage has to be empty is sem allowed
+												  //not necessary to search AC first
+			if (perform_on_type(msc, ac_stages, acoustic_stg_cnt) == -1)
+				perform_on_type(msc, ec_stages, electric_stg_cnt);
+			sem_post(&both_sem);
+			performed = 1;
+		}
+	}
+
+	if (!performed) {
+		printf(RED_COL "%s %s leaving because of impatience !!\n" RST_COL, msc->name, instr_name[msc->instr]);
+		return NULL;
+	}
+	// collect tshirt
+}
+
+int perform_on_type(musician_t *msc, stage_t *stages, int stg_cnt) {
+	int i;
+	if (msc->instr != SINGER) {
+		for (i = 0; i < stg_cnt; i++) {
+			pthread_mutex_lock(stages[i].mutex);
+			//assuming musicians enter completely empty stage
+			if (!stages[i].musician_performing && !stages[i].singer_performing) {
+				stages[i].musician_performing = 1;
+				pthread_mutex_unlock(&(stages[i].mutex));
+				int perform_time = randInt(t1, t2);
+				int singer_joined = 0;
+				clock_t performance_st_time = clock();
+				while (clock() - performance_st_time < perform_time * CLOCKS_PER_SEC) {
+						if( !singer_joined  && stages[i].singer_performing]){
+							singer_joined = 1;
+							perform_time += 2;
+						}
+				}
+				break;
+			}
+			pthread_mutex_unlock(&(stages[i].mutex));
+		}
+	} else {
+		for (int i = 0; i < stg_cnt; i++) {
+			pthread_mutex_lock(stages[i].mutex);
+			if (!stages[i].singer_performing) {
+				stages[i].singer_performing = 1;
+				pthread_mutex_unlock(&(stages[i].mutex));
+				int musician_there = stages[i].musician_performing;
+				while (stages[i].musician_performing)
+					;
+			}
+			pthread_mutex_unlock(&(stages[i].mutex));
+		}
+	}
+	if (i == stg_cnt)
+		return -1;
+	return i;
+}
+
+#endif
